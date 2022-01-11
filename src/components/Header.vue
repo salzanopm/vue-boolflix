3a0bd9e4f37c3b2ace21b50b1563938d918b6af4
salzanopm <salzanopm@gmail.com>
2022-01-11 16:41:55 +0100

    scaffolding

---
+++ src/components/Header.vue
@@ -0,0 +1,19 @@
+<template>
+  <header>
+      <input type="text" placeholder="cerca">
+      <button>enter</button>
+  </header>
+</template>
+
+<script>
+
+export default {
+    name:'Header',
+}
+</script>
+
+<style scoped>
+
+
+
+</style>
---
+++ src/components/Main.vue
@@ -0,0 +1,27 @@
+<template>
+  <main>
+     <ul>
+         <li>ciao</li>
+         <li>ciao</li>
+     </ul>
+  </main>
+</template>
+
+<script>
+export default {
+    name:'Main',
+}
+</script>
+
+<style scoped>
+ul {
+    display: flex;
+    flex-wrap: wrap;
+
+}
+    li {
+        list-style-type: none;
+        width:calc(100% / 4);
+    }
+
+</style>
