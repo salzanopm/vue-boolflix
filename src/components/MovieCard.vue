4d60c801d4571cbe57fd05fa12a6126ab42ddffc
salzanopm <salzanopm@gmail.com>
2022-01-12 19:59:24 +0100

    Milestone-2 end

--- src/components/MovieCard.vue
+++ src/components/MovieCard.vue
@@ -2,7 +2,15 @@
     <div class="movie">
         <div>Titolo: {{details.title}}</div>
         <div>Titolo originale: {{details.original_title}}</div>
-        <div>Lingua: {{details.original_language}}</div>
+        <!-- se bandiera inclusa in array, usa immagine -->
+        <div v-if="flagList.includes(details.original_language)">
+            Lingua:
+            <img :src="require(`../assets/${details.original_language}.png`)" class="img-language" alt="">
+        </div>
+        <!-- altrimenti stampa normalmente -->
+        <div v-else>
+            Lingua: {{details.original_language}}
+        </div>
         <div>Voto: {{details.vote_average}}</div>
     </div>
 
@@ -13,6 +21,11 @@ export default {
     name: 'MovieCard',
     props: {
         details: Object
+    },
+    data: function() {
+        return {
+            flagList: ['it','en']
+        }
     }
 }
 </script>
@@ -21,5 +34,9 @@ export default {
 .movie {
     margin: 20px;
     border: 2px solid black;
+}
+
+.img-language {
+    width: 10px;
 }
 </style>
--- src/components/SeriesCard.vue
+++ src/components/SeriesCard.vue
@@ -2,7 +2,15 @@
     <div class="series">
         <div>Titolo: {{details.name}}</div>
         <div>Titolo originale: {{details.original_name}}</div>
-        <div>Lingua: {{details.original_language}}</div>
+        <!-- se bandiera inclusa in array, usa immagine -->
+        <div v-if="flagList.includes(details.original_language)">
+            Lingua:
+            <img :src="require(`../assets/${details.original_language}.png`)" class="img-language" alt="">
+        </div>
+        <!-- altrimenti stampa normalmente -->
+        <div v-else>
+            Lingua: {{details.original_language}}
+        </div>
         <div>Voto: {{details.vote_average}}</div>
     </div>
 
@@ -13,6 +21,11 @@ export default {
     name: 'SeriesCard',
     props: {
         details: Object
+    },
+    data: function() {
+        return {
+            flagList: ['it','en']
+        }
     }
 }
 </script>
@@ -21,5 +34,9 @@ export default {
 .series {
     margin: 20px;
     border: 2px solid black;
+}
+
+.img-language {
+    width: 10px;
 }
 </style>
