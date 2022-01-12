5bab7112adaec25b3f6433f85360b8b8fac4e1fe
salzanopm <salzanopm@gmail.com>
2022-01-12 20:36:47 +0100

    Milestone-3

--- src/components/MovieCard.vue
+++ src/components/MovieCard.vue
@@ -1,22 +1,27 @@
 <template>
+<div>
     <div>
+        <img :src="`https://image.tmdb.org/t/p/${imgWidthMovie}${details.poster_path}`" alt="">
+    </div>
+    <div class="movie">
+        <div>Titolo: {{details.title}}</div>
+        <div>Titolo originale: {{details.original_title}}</div>
+        <!-- se bandiera inclusa in array, usa immagine `-->
+        <div v-if="flagListMovie.includes(details.original_language)">
+            Lingua:
+            <img :src="require(`../assets/${details.original_language}.png`)" class="img-language" alt="">
+        </div>
+        <!-- altrimenti stampa normalmente -->
+        <div v-else>
+            Lingua: {{details.original_language}}
+        </div>
         <div>
-            <img :src="`https://image.tmdb.org/t/p/${imgWidth}${details.poster_path}`" alt="">
+            Voto:
+            <i class="fas fa-star" v-for="number,index in getNumberOfStars(details.vote_average)" :key='index'></i>
+            <i class="far fa-star" v-for="number,index in (5 - getNumberOfStars(details.vote_average))" :key='index'></i>
+        <!-- <div>Voto: {{details.vote_average}}</div> -->
         </div>
-        <div class="movie">
-            <div>Titolo: {{details.title}}</div>
-            <div>Titolo originale: {{details.original_title}}</div>
-            <!-- se bandiera inclusa in array, usa immagine `-->
-            <div v-if="flagList.includes(details.original_language)">
-                Lingua:
-                <img :src="require(`../assets/${details.original_language}.png`)" class="img-language" alt="">
-            </div>
-            <!-- altrimenti stampa normalmente -->
-            <div v-else>
-                Lingua: {{details.original_language}}
-            </div>
-            <div>Voto: {{details.vote_average}}</div>
-        </div>
+    </div>
 </div>
 </template>
 
@@ -26,10 +31,16 @@ export default {
     props: {
         details: Object
     },
+    methods: {
+        getNumberOfStars: function(vote) {
+            return Math.ceil(vote/2);
+        }
+        
+    },
     data: function() {
         return {
-            flagList: ['it','en'],
-            imgWidth: 'w500'
+            flagListMovie: ['it','en'],
+            imgWidthMovie: 'w500'
         }
 
     }
--- src/components/SeriesCard.vue
+++ src/components/SeriesCard.vue
@@ -1,9 +1,13 @@
 <template>
+<div>
+    <div>
+        <img :src="`https://image.tmdb.org/t/p/${imgwidthSeries}${details.poster_path}`" alt="">
+    </div>
     <div class="series">
         <div>Titolo: {{details.name}}</div>
         <div>Titolo originale: {{details.original_name}}</div>
         <!-- se bandiera inclusa in array, usa immagine -->
-        <div v-if="flagList.includes(details.original_language)">
+        <div v-if="flagListSeries.includes(details.original_language)">
             Lingua:
             <img :src="require(`../assets/${details.original_language}.png`)" class="img-language" alt="">
         </div>
@@ -11,9 +15,14 @@
         <div v-else>
             Lingua: {{details.original_language}}
         </div>
-        <div>Voto: {{details.vote_average}}</div>
+        <div>
+            Voto:
+            <i class="fas fa-star" v-for="number,index in getNumberOfStars(details.vote_average)" :key='index'></i>
+            <i class="far fa-star" v-for="number,index in (5 - getNumberOfStars(details.vote_average))" :key='index'></i>
+        <!-- <div>Voto: {{details.vote_average}}</div> -->
+        </div>
     </div>
-
+</div>
 </template>
 
 <script>
@@ -24,9 +33,17 @@ export default {
     },
     data: function() {
         return {
-            flagList: ['it','en']
+            flagListSeries: ['it','en'],
+            imgwidthSeries: 'w500'
         }
-    }
+    },
+    methods: {
+        getNumberOfStars: function(vote) {
+            return Math.ceil(vote/2);
+        }
+        
+    },
+   
 }
 </script>
 
